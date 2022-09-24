429ccaf67c50a147022416b3ce79f4379e014321
TechnicallyRae <113115235+TechnicallyRae@users.noreply.github.com>
2022-09-23 22:20:04 -0400

    updating alert
    changing message for alert, repetitive message on double alerts, changed secondary

--- OnePager-Introduction/onepager.js
+++ OnePager-Introduction/onepager.js
@@ -2,5 +2,5 @@ window.alert("Bye, for now! Thanks for visiting!")
 
 document.getElementById('bye').addEventListener ('click', myFunction); 
 function myFunction() {
-    alert ("Thanks for Visiting!")
+    alert ("Have a good day!")
  }
